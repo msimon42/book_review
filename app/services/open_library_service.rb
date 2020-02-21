2fb58a4c0cad029c3cd3568c7da678315fe60b83
msimon42 <msimon@uab.edu>
2020-02-21 10:11:46 -0700

    Add open library service

--- app/services/open_library_service.rb
+++ app/services/open_library_service.rb
@@ -1,21 +1,22 @@
 class OpenLibraryService
   def book_search(title)
-    result = get_json({title: title})
+    result = get_json('https://openlibrary.org/search.json',{title: title})
     output = {
-      title: result['docs']['title'],
-      author: result['docs']['author'],
-      genre: result['docs']['genre']
+      title: result['docs'][0]['title'],
+      author: result['docs'][0]['author_name'][0],
+      genre: result['docs'][0]['type']
     }
+  end
 
   private
     def connection
       Faraday.new(url: 'https://openlibrary.org/search.json') do |f|
-        f.default_adapter Faraday.default_adapter
+        f.adapter Faraday.default_adapter
       end
     end
 
-    def get_json(params)
-      response = connection.get(params)
-      json.parse(response.body)
+    def get_json(url, params)
+      response = connection.get(url, params)
+      JSON.parse(response.body)
     end
 end
